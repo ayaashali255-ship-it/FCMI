3e686d1478cbf81416f0c8129a49f98998de68a4
ayaashali255-ship-it <ayaashali255@gmail.com>
2026-06-28 19:21:11 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,8 +7,24 @@ async function loadTactics() {
         const response = await fetch("data/tactics.json");
         tactics = await response.json();
 
-        console.log("FCMI Database Loaded");
-        console.log(tactics);
+        const container = document.getElementById("tactics-container");
+
+        if (!container) return;
+
+        container.innerHTML = "";
+
+        tactics.forEach(tactic => {
+            container.innerHTML += `
+                <div class="card">
+                    <h3>${tactic.name}</h3>
+                    <p><b>ID:</b> ${tactic.id}</p>
+                    <p><b>Formation:</b> ${tactic.formation}</p>
+                    <p><b>Game Mode:</b> ${tactic.gameMode}</p>
+                    <p><b>Rating:</b> ${tactic.rating}/100</p>
+                </div>
+            `;
+        });
+
     } catch (error) {
         console.error("Database Error:", error);
     }
