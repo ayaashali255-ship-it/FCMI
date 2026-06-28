cc49e22a793b31d27bf92a0785b72f97d6a7d0f6
ayaashali255-ship-it <ayaashali255@gmail.com>
2026-06-28 19:00:53 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-// FCMI JavaScript
+// FCMI Database Engine
+
+let tactics = [];
+
+async function loadTactics() {
+    try {
+        const response = await fetch("data/tactics.json");
+        tactics = await response.json();
+
+        console.log("FCMI Database Loaded");
+        console.log(tactics);
+    } catch (error) {
+        console.error("Database Error:", error);
+    }
+}
+
+loadTactics();
